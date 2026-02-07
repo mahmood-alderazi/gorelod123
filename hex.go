2f257e68e7f11749ce28502a984875df7af4f5db
malderaz <Ald.mahmood@outlook.com>
2026-02-07 07:26:16 +0300

    done

--- hex.go
+++ hex.go
@@ -10,17 +10,15 @@ func convertHex(text string) string {
 	words := strings.Split(text, " ") //["Simply", "add", "1E", "(hex)"]
 
 	for i, value := range words {
-		if value == "(hex)" {
+		if value == "(hex)" && i > 0 {
 
 			back := words[i-1]
 
-			//covert back to intger
-
 			decimal, _ := strconv.ParseInt(back, 16, 64)
 
-			text = strconv.Itoa(int(decimal))
+			words[i-1] = strconv.Itoa(int(decimal))
+			words[i] = ""
 		}
 	}
-
-	return text
+	return strings.Join(words, " ")
 }
